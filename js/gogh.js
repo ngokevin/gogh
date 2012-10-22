62bbfac59767bc3fbbf43a9500e501ad7de168c3
Kevin Ngo <ktngo09@gmail.com>
2012-10-22 18:07:48 +0000

    a shadow on the stroke

--- js/gogh.js
+++ js/gogh.js
@@ -17,20 +17,20 @@ $(document).ready(function() {
     var h = canvas.height;
 
     function gogh() {
-        // Update stage.
-        y += 20;
-
-        // Clear stage.
         // ctx.clearRect(0, 0, canvas.width, canvas.height);
 
         // Render stage.
-        ctx.fillStyle = 'rgb(40, 40, 220)';
+        ctx.fillStyle = ctx.shadowColor = 'rgb(80, 40, 200)';
+        ctx.shadowBlur = 5;
         ctx.beginPath();
         ctx.moveTo(w / 2, y);
         ctx.lineTo(w / 2, y + 20);
         ctx.lineTo(w / 2 + 20, y + 20);
         ctx.lineTo(w / 2 + 20, y);
         ctx.fill();
+
+        // Update stage.
+        y += 20;
 
         // Request new frame.
         requestAnimFrame(function() {
